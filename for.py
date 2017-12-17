7f2b1306602652110894c58e9ed89f73ab5a721f
ashish <ashishbisht723@gmail.com>
2017-12-17 23:08:38 +0530

    for

---
+++ for.py
@@ -0,0 +1,17 @@
+# for loop simple example
+
+l = ['ashish','popu',1,2,3,4,5,6]
+
+for val in l:
+    print(val)
+
+    
+
+# one more example 
+sequence = range(0,40)
+for seq in sequence:
+    if seq%2==0 and seq>0:
+        print("The number is : ",seq,"and it is even number")
+else:
+    print("the number is 0")
+
