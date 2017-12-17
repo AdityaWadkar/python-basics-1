324e49727d8514d59dee27f164a83b7ab031b670
ashish <ashishbisht723@gmail.com>
2017-12-17 23:05:44 +0530

    array pair sum

---
+++ array_pair_sum.py
@@ -0,0 +1,9 @@
+#array pair sum problem ,lets go .............
+key = input("Enter key for whom you want to check array pair sum :")
+val = input('enter {} numbers'.format(key))
+print(val)
+pair_list = [i for i in val] 
+print(pair_list)
+# def sum():
+    
+#     pair_sum = ([1,3,2,2],4)
