89d48da6b560f280220e10d9cee73363d38bc8e8
AdityaWadkar <67093170+AdityaWadkar@users.noreply.github.com>
2020-10-02 17:06:20 +0530

    Update for.py

--- for.py
+++ for.py
@@ -15,3 +15,12 @@ for seq in sequence:
 else:
     print("the number is 0")
 
+#Heart Pattern using for loop
+for row in range(6):
+    for col in range(7):
+        if(row==0 and col %3==1)or(row==1 and col%3==0)or(row-col==2)or(row + col==8):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+
